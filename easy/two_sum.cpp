28ab86c15f70b63b2f870e60f380cf919ae6da73
hrittik16 <hrittikdas16@gmail.com>
2020-01-15 16:17:01 +0530

    two sum updated

--- easy/two_sum.cpp
+++ easy/two_sum.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, int> indices;
+        unordered_map<int, int> indices;
         vector<int> pair_index;
         for(int i = 0; i < nums.size(); i++) { // O(N)
-            if(indices.find(target-nums[i]) == indices.end()) { // O(log(N))
+            if(indices.find(target-nums[i]) == indices.end()) { // O(1)
                 indices[nums[i]] = i;
             }
             else {
