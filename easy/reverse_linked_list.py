394605ca1e0b7afe4895903bd3d0e99d6d60d0de
hrittik16 <hrittikdas16@gmail.com>
2020-02-05 23:22:26 +0530

    delete_node_in_linkedlist.py added

--- easy/reverse_linked_list.py
+++ easy/reverse_linked_list.py
@@ -7,19 +7,20 @@
 
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        if head is None:
-            return head
         # Iterative Solution
-        prev = None
-        curr = head
-        next = head.next
-        while next is not None:
-            curr.next = prev
-            prev = curr
-            curr = next
-            next = next.next
-        curr.next = prev
-        prev = curr
-        curr = next
-        head = prev
-        return head
+        #if head is None:
+        #    return head
+        #prev = None
+        #curr = head
+        #next = head.next
+        #while next is not None:
+        #    curr.next = prev
+        #    prev = curr
+        #    curr = next
+        #    next = next.next
+        #curr.next = prev
+        #prev = curr
+        #curr = next
+        #head = prev
+        #return head
+
